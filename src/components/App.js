8fbcfb51352f77fdb9e3af6bbbf0ff282723fcc7
Kishanvaghasiya108 <kishanvaghasiya2008@gmail.com>
2023-04-08 12:28:08 +0530

    updated app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,55 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/App.css";
 import User from "../models/user";
 
 const App = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [confirm, setConfirm] = useState("");
+  const [loginMail, setLoginMail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const [btn, setBtn] = useState(false);
+  const [visible,setVisible] = useState(false);
+
+  const singupHandler = () => {
+    if(pass === confirm){
+      setVisible(true);
+
+    }
+
+  };
+  const loginHandler = () => {
+    if (email === loginMail && pass === loginPassword) {
+      setBtn(true);
+    }
+  };
+  const logoutHandler = () => {
+    setBtn(false);
+  };
+
   return (
     <div id="main">
       <table id="all-users">
-      <tbody>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Password</th>
-          </tr>
-          <tr>
-            <td>{/** user's name */}</td>
-            <td>{/** user's email */}</td>
-            <td>{/** user's password */}</td>
-          </tr>
-        </tbody>
-      </table>
+            <tbody>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Password</th>
+              </tr>
+              {visible ?<tr>
+                <td>{name}</td>
+                <td>{email}</td>
+                <td>{pass}</td>
+              </tr> : null}
+            </tbody>
+          </table>
+      {btn === false ? (
+        <>
+      
+          
+          
 
-      <div>
-        <form className="signup-form">
-          <label htmlFor="name">Name</label>
-          <input type="text" name="signupName" id="signupName" />
-          <label htmlFor="email">Email</label>
-          <input type="email" name="signupEmail" id="signupEmail" />
-          <label htmlFor="password">Password</label>
-          <input type="password" name="signupPassword" id="signupPassword" />
-          <label htmlFor="confirmPassword">Confirm Password</label>
-          <input
-            type="password"
-            name="signupConfirmPassword"
-            id="signupConfirmPassword"
-          />
-        </form>
-        <button id="signup-button">Signup</button>
-        <form className="login-styles">
-          <label htmlFor="loginEmail">Email</label>
-          <input id="loginEmail" name="loginEmail" type="email" />
-          <label htmlFor="loginPassword">Password</label>
-          <input id="loginPassword" name="loginPassword" type="password" />
-        </form>
-        <button id="login-button">Login</button>
-      </div>
-
-      <div>
-        <h3 id="username">{/** Logged in user's name */}</h3>
-        <h3 id="email">{/** Logged in user's email */}</h3>
-        <button id="logout-button">Logout</button>
-      </div>
+          <div>
+            <form className="signup-form">
+              <label htmlFor="name">Name</label>
+              <input type="text" name="signupName" id="signupName" onChange={(e)=> setName(e.target.value)}/>
+              <label htmlFor="email">Email</label>
+              <input type="email" name="signupEmail" id="signupEmail" onChange={(e)=>setEmail(e.target.value)}/>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                name="signupPassword"
+                id="signupPassword"
+                onChange={(e)=>setPass(e.target.value)}
+              />
+              <label htmlFor="confirmPassword">Confirm Password</label>
+              <input
+                type="password"
+                name="signupConfirmPassword"
+                id="signupConfirmPassword"
+                onChange={(e)=>setConfirm(e.target.value)}
+              />
+            </form>
+            <button id="signup-button" onClick={singupHandler}>
+              Signup
+            </button>
+            <form className="login-styles">
+              <label htmlFor="loginEmail">Email</label>
+              <input id="loginEmail" name="loginEmail" type="email" onChange={(e)=>setLoginMail(e.target.value)}/>
+              <label htmlFor="loginPassword">Password</label>
+              <input id="loginPassword" name="loginPassword" type="password" onChange={(e)=>setLoginPassword(e.target.value)}/>
+            </form>
+            <button id="login-button" onClick={loginHandler}>
+              Login
+            </button>
+          </div>
+        </>
+      ) : (
+        <div>
+          <h3 id="username">{name}</h3>
+          <h3 id="email">{email}</h3>
+          <button id="logout-button" onClick={logoutHandler}>
+            Logout
+          </button>
+        </div>
+      )}
     </div>
   );
 };
